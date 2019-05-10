53e0ba13bfebad5a335d5788d907f17651df5790
FilipovskyAlex <filipovskiy99@gmail.com>
2019-05-10 17:34:42 +0300

    save user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterUserRequest;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function register(RegisterUserRequest $request)
     {
+        $user = new User;
+
+        $user->username = $request->get('username');
+        $user->email = $request->get('email');
+        $user->password = Hash::make($request->get('password'));
+
+        $user->save();
+
         //
     }
 }
